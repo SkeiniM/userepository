83a2130c679c84a8e09b8af0678b0392604017aa
SkeiniM <SkeiniM@users.noreply.github.com>
2025-09-26 01:17:02 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="SkeiniM"
+original_name="userepository"
+original_urlname="userepository"
+original_description="Awesome userepository created by SkeiniM"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv userepository $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ userepository/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for userepository."""
+
+from userepository.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
